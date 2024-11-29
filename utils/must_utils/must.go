13c405bd7c487f9f4e6dacd7f845ba37cebd7b6a
Peter Ibragimov <pibragimov@mts.ru>
2024-11-29 20:01:23 +0300

    feat: add err to panic

--- utils/must_utils/must.go
+++ utils/must_utils/must.go
@@ -1,14 +1,16 @@
 package must_utils
 
+import "fmt"
+
 func Must[T any](obj T, err error) T {
 	if err != nil {
-		panic("must failed")
+		panic(fmt.Errorf("must failed: %v", err))
 	}
 	return obj
 }
 
 func MustNoReturn(err error) {
 	if err != nil {
-		panic("must failed")
+		panic(fmt.Errorf("must failed: %v", err))
 	}
 }
