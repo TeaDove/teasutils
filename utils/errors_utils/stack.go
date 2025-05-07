69158d663b8638d21d386800e71402ef77fd12a3
Peter Ibragimov <pibragimov@mts.ru>
2025-05-07 20:55:31 +0300

    feat: add with stack if required

---
+++ utils/errors_utils/stack.go
@@ -0,0 +1,14 @@
+package errors_utils
+
+import "github.com/pkg/errors"
+
+func WithStackIfRequired(err error) error {
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
+	_, ok := err.(stackTracer)
+	if ok {
+		return err
+	}
+	return errors.WithStack(err)
+}
