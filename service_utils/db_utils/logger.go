599726fff9653a7b10b978488bb9726ecb4d67ce
TeaDove <peter.ibragimov@gmail.com>
2025-12-19 15:53:56 +0400

    daily small edit from TeaDoves-MacBook-Air-M4.local

---
+++ service_utils/db_utils/logger.go
@@ -0,0 +1,39 @@
+package db_utils
+
+import (
+	"context"
+	"time"
+
+	"github.com/rs/zerolog"
+	"gorm.io/gorm/logger"
+)
+
+type ZerologAdapter struct {
+}
+
+func (z ZerologAdapter) LogMode(level logger.LogLevel) logger.Interface {
+	panic("no implemented")
+}
+
+func (z ZerologAdapter) Info(ctx context.Context, s string, i ...interface{}) {
+	zerolog.Ctx(ctx).Info().Msgf(s, i)
+}
+
+func (z ZerologAdapter) Warn(ctx context.Context, s string, i ...interface{}) {
+	zerolog.Ctx(ctx).Warn().Msgf(s, i)
+}
+
+func (z ZerologAdapter) Error(ctx context.Context, s string, i ...interface{}) {
+	zerolog.Ctx(ctx).Error().Msgf(s, i)
+}
+
+func (z ZerologAdapter) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	sql, rowAffected := fc()
+
+	zerolog.Ctx(ctx).Trace().
+		Int64("row_affected", rowAffected).
+		Str("sql", sql).
+		Time("begin", begin).
+		Err(err).
+		Msgf("gorm.trace")
+}
--- service_utils/go.mod
+++ service_utils/go.mod
@@ -13,9 +13,13 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
