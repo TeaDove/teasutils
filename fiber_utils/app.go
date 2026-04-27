8aa4c73b771f5d5e095b9954043c2cf36ff44c34
TeaDove <peter.ibragimov@gmail.com>
2026-04-27 21:12:46 +0300

    evening minor edit from Mac.bbrouter

--- fiber_utils/app.go
+++ fiber_utils/app.go
@@ -1,6 +1,10 @@
 package fiber_utils
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/cockroachdb/errors"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v3"
+)
 
 type StructValidator struct {
 	Validator *validator.Validate
@@ -12,4 +16,23 @@ func (v *StructValidator) Validate(out any) error {
 
 func NewDefaultStructValidator() *StructValidator {
 	return &StructValidator{validator.New(validator.WithRequiredStructEnabled())}
+}
+
+func BindJSON[T any](c fiber.Ctx) (T, error) {
+	var req T
+
+	err := c.Bind().JSON(&req)
+	if err != nil {
+		return *new(T), SendUnprocessable(errors.Wrap(err, "failed to bind JSON body"))
+	}
+
+	return req, nil
+}
+
+func SendUnprocessable(err error) error {
+	return &fiber.Error{Code: fiber.StatusUnprocessableEntity, Message: err.Error()}
+}
+
+func SendBadRequest(err error) error {
+	return &fiber.Error{Code: fiber.StatusBadRequest, Message: err.Error()}
 }
